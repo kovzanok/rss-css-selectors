9ca8d5c56f814a10ee0699a512b4021c83fc83e1
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-12 18:59:38 +0300

    feat: implement create element function to navigation component

--- rss-css-selectors/src/app/Navigation/Navigation.ts
+++ rss-css-selectors/src/app/Navigation/Navigation.ts
@@ -1,52 +1,66 @@
 import { Task } from '../../types';
 import GameBlock from '../../utils/GameBlock';
 import { levels } from '../../levels';
+import { createElement } from '../../utils/utils';
 
 export default class Navigation extends GameBlock {
   constructor(task: Task, private levelNum: number) {
     super({ task });
   }
 
-  renderNavigation(): HTMLDivElement {
-    const navigation = document.createElement('div');
+  public renderNavigation(): HTMLDivElement {
+    const navigation = createElement<HTMLDivElement>({ tag: 'div' });
 
     const buttons = this.renderControls();
 
-    const levelCount = document.createElement('h2');
-    levelCount.textContent = `Level ${this.levelNum + 1} of ${levels.length}`;
+    const levelCount = createElement<HTMLHeadingElement>({
+      tag: 'h2',
+      textContent: `Level ${this.levelNum + 1} of ${levels.length}`,
+    });
 
-    if (this.task) {
-      const title = document.createElement('h3');
-      title.textContent = this.task.taskName;
-
-      const text = document.createElement('p');
-      text.textContent = this.task.taskText;
-
-      const description = document.createElement('p');
-      description.textContent = this.task.taskDescription;
-
-      const examples = document.createElement('p');
-      examples.textContent = `Examples:\n${this.task.examples}`;
-
-      navigation.append(...buttons, levelCount, title, text, description, examples);
-    }
-
+    navigation.append(...buttons, levelCount, ...this.renderTaskFullInfo());
     return navigation;
   }
 
-  renderControls() {
+  private renderControls(): HTMLButtonElement[] {
     const controls = ['prev-lvl', 'next-lvl'];
     return controls.map((control: string): HTMLButtonElement => {
-      const button = document.createElement('button');
-      button.className = control;
-      button.textContent = control;
-      button.disabled =
+      const callback =
+        control === 'prev-lvl' ? this.controller.prevLevel : this.controller.nextLevel;
+      const isDisabled =
         (control === 'prev-lvl' && this.levelNum === 0) ||
         (control === 'next-lvl' && this.levelNum === levels.length - 1);
-      button.onclick =
-        control === 'prev-lvl' ? this.controller.prevLevel : this.controller.nextLevel;
-
+      const button = createElement<HTMLButtonElement>({
+        tag: 'button',
+        className: control,
+        disabled: isDisabled,
+        textContent: control,
+        eventHandlers: { click: callback },
+      });
       return button;
     });
+  }
+
+  private renderTaskFullInfo(): HTMLElement[] {
+    if (this.task) {
+      const title = createElement<HTMLHeadingElement>({
+        tag: 'h3',
+        textContent: this.task.taskName,
+      });
+
+      const text = createElement<HTMLParagraphElement>({
+        tag: 'p',
+        textContent: this.task.taskText,
+      });
+
+      const description = createElement<HTMLParagraphElement>({
+        tag: 'p',
+        textContent: this.task.taskDescription,
+      });
+
+      const examples = createElement({ tag: 'p', textContent: `Examples:\n${this.task.examples}` });
+      return [title, text, description, examples];
+    }
+    return [];
   }
 }
