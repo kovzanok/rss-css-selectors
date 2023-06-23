a7e7b8530a67559fb99ef18e83419b6de8e529b0
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-23 19:10:38 +0300

    feat: add test of hightlightElements Model method

--- rss-css-selectors/src/app/Model/Model.test.ts
+++ rss-css-selectors/src/app/Model/Model.test.ts
@@ -1,4 +1,4 @@
-import Model from "./Model";
+import Model from './Model';
 
 describe('Model.getElementMarkup', () => {
   const element = document.createElement('div');
@@ -15,5 +15,18 @@ describe('Model.getElementMarkup', () => {
     element.append(document.createElement('div'));
     expect(Model.getElementMarkup(element)).toBe('<tag className="className"></tag>');
     element.innerHTML = '';
+  });
+});
+
+describe('Model.hightlightElements', () => {
+  test('hightlightElements', () => {
+    document.body.innerHTML = `<div></div><span></span>`;
+    const firstElement = document.querySelector('div') as HTMLDivElement;
+    const secondElement = document.querySelector('span') as HTMLSpanElement;
+    Model.hightlightElements(firstElement, secondElement);
+
+    expect(document.body.innerHTML).toBe(
+      '<div class="highlighten"></div><span class="highlighten"></span>'
+    );
   });
 });
