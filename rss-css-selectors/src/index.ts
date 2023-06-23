f4bff888581b83a7fc07ec005eb5fced500f354d
Alex Kovzan <alexanderkovzan@gmail.com>
2023-06-23 22:58:06 +0300

    feat: remove store subscription

--- rss-css-selectors/src/index.ts
+++ rss-css-selectors/src/index.ts
@@ -2,12 +2,6 @@ import App from './app/App';
 import './style/animation.scss';
 import './style/placeholders.scss';
 import './style/index.scss';
-import store from './redux/store';
 
-console.log('Initial state: ', store.getState());
-
-store.subscribe(() => {
-  console.log('State: ', store.getState());
-});
 const app = new App();
 app.start();
